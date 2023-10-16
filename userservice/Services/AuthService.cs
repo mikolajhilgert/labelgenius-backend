38e53657603045ac4e6c7ca715f50c597e5446a8
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2023-10-16 18:07:58 +0200

    First version of the user micro-service

---
+++ userservice/Services/AuthService.cs
@@ -0,0 +1,59 @@
+﻿using Firebase.Auth;
+using System.Net.Http.Headers;
+using userservice.Dto;
+
+namespace userservice.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _config;
+        private readonly IFirebaseAuthClient _firebaseAuthClient;
+        private readonly Repositories.IUserRepository _userRepository;
+        public AuthService(IConfiguration config, IFirebaseAuthClient firebaseAuthClient, Repositories.IUserRepository userRepository)
+        {
+            this._config = config;
+            this._firebaseAuthClient = firebaseAuthClient;
+            this._userRepository = userRepository;
+        }
+        public async Task<Tuple<bool, string, FirebaseCredential>> LoginUser(UserLoginDto userDto)
+        {
+            try
+            {
+                var auth = await _firebaseAuthClient.SignInWithEmailAndPasswordAsync(userDto.Email, userDto.Password);
+                if (!auth.User.Info.IsEmailVerified)
+                {
+                    return new Tuple<bool, string, FirebaseCredential>(false, "Your email address has not been verified", new());
+                }
+
+                return new Tuple<bool, string, FirebaseCredential>(true, "Sign-in successful", auth.User.Credential);
+            }
+            catch (Exception e)
+            {
+                return new Tuple<bool, string, FirebaseCredential>(false, e.Message, new());
+            }
+        }
+
+        public async Task<Tuple<bool, string>> RegisterUser(UserRegisterDto userDto)
+        {
+            UserCredential firebaseUserCredential = await _firebaseAuthClient.CreateUserWithEmailAndPasswordAsync(userDto.Email, userDto.Password);
+            await SendVerificationEmailAsync(firebaseUserCredential);
+            return await _userRepository.SaveUser(userDto, firebaseUserCredential.User.Uid);
+        }
+
+        private async Task SendVerificationEmailAsync(UserCredential firebaseUserCredential)
+        {
+            string tokenId = await firebaseUserCredential.User.GetIdTokenAsync();
+            string RequestUri = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=" + _config.GetSection("FirebaseSettings:apiKey").Value;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var content = new StringContent("{\"requestType\":\"VERIFY_EMAIL\",\"idToken\":\"" + tokenId + "\"}");
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                var response = await client.PostAsync(RequestUri, content);
+                response.EnsureSuccessStatusCode();
+            }
+        }
+    }
+}
