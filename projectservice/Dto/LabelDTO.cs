ef3985822c5606aa686875658ffa46b98cde63f1
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2023-11-24 00:06:51 +0100

    Make labelling service woek

--- projectservice/Dto/LabelDTO.cs
+++ projectservice/Dto/LabelDTO.cs
@@ -2,54 +2,23 @@
 
 namespace projectservice.Dto
 {
-    public class CreateLabelDTO
+    public class ImageLabelsDTO
     {
         [Required]
         public string ProjectId { get; set; } = string.Empty;
         [Required]
         public string ImageId { get; set; } = string.Empty;
         [Required]
-        public double X { get; set; } = 0.0;
-        [Required]
-        public double Y { get; set; } = 0.0;
-        [Required]
-        public double Height { get; set; } = 0.0;
-        [Required]
-        public double Width { get; set; } = 0.0;
-        [Required]
-        public string ClassName { get; set; } = string.Empty;
+        public List<LabelDTO> Labels { get; set; } = new();
         public string Creator { get; set; } = string.Empty;
     }
-    public class UpdateLabelDTO
+    public class LabelDTO
     {
-        [Required]
         public string Id { get; set; } = string.Empty;
-        [Required]
-        public string ProjectId { get; set; } = string.Empty;
-        [Required]
-        public string ImageId { get; set; } = string.Empty;
-        [Required]
-        public double X { get; set; } = 0.0;
-        [Required]
-        public double Y { get; set; } = 0.0;
-        [Required]
-        public double Height { get; set; } = 0.0;
-        [Required]
-        public double Width { get; set; } = 0.0;
-        [Required]
         public string ClassName { get; set; } = string.Empty;
-        public string Creator { get; set; } = string.Empty;
-    }
-    public class ResponseLabelDTO
-    {
-        public string Id { get; set; } = string.Empty;
-        public string ProjectId { get; set; } = string.Empty;
-        public string ImageId { get; set; } = string.Empty;
         public double X { get; set; } = 0.0;
         public double Y { get; set; } = 0.0;
         public double Height { get; set; } = 0.0;
         public double Width { get; set; } = 0.0;
-        public string ClassName { get; set; } = string.Empty;
-        public string Creator { get; set; } = string.Empty;
     }
 }
