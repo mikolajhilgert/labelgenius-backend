395c826c264e50e3331667d0fce9ff846d09be79
Mikolaj Hilgert <mikolaj.hilgert@protonmail.com>
2023-11-26 21:17:53 +0100

    Add unit-tests

---
+++ projectservice.unittests/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using projectservice.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectservice.unittests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void FakeTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
+
